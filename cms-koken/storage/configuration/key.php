e52bea76e8910a4c27d073df381a4e668ba7c0bb
Viktor Breitenbuecher <vic.b@outlook.com>
2016-07-24 14:16:06 +0200

    Install koken cms.

---
+++ cms-koken/storage/configuration/key.php
@@ -0,0 +1,6 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// Do not edit or remove this file unless advised by Koken support
+// support@koken.me
+
+return 'a11576f71fa4935b874408f081caddb7';
